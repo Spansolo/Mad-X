73dffe995c59c1312e04f37b3847f96d0f314dde
Spansolo <hannah.norman@manchester.ac.uk>
2024-08-20 08:03:21 +1000

    survival plotting python file

--- Tol_study/plotk2lk3l.py
+++ Tol_study/plotk2lk3l.py
@@ -23,7 +23,17 @@ print('S',S,'O',O)
 print('shape',np.shape(S))
 
 
+SMALL_SIZE = 18
+MEDIUM_SIZE = 16
+BIGGER_SIZE = 16
 
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=18)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
 sur_1=(np.loadtxt('120624_1sthalf_survived_30_natchrom.txt'))
 sur_2=(np.loadtxt('120624_2ndhalf_survived_30_natchrom.txt'))
@@ -32,14 +42,14 @@ sur_4=(np.loadtxt('120624_4thhalf_survived_30_natchrom.txt'))
 
 boo=[sur_1,sur_2,sur_3,sur_4]
 print(len(boo))
-
+print(boo)
 
 #survival plot
 plt.clf()
 plt.scatter(O,S,marker="s",c=boo,s=900,cmap='turbo')
 plt.xlabel('K3L ')
 plt.ylabel('K2L ')
-plt.colorbar(label='Number survived')
+plt.colorbar(label='Number of Initial Amplitudes with 100% survival')
 plt.show()
-plt.savefig('120624_k2lk3l_30natchrom_16.png')
+plt.savefig('200824_k2lk3l_30natchrom_100.png')
 
